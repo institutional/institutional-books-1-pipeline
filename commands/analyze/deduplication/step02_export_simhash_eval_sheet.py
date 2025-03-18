d6d1a3c418d2032f60f6056738b3a66d0a93cea6
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2025-03-18 14:06:06 -0400

    Progress: analyze commands group

--- commands/analyze/deduplication/step02_export_simhash_eval_sheet.py
+++ commands/analyze/deduplication/step02_export_simhash_eval_sheet.py
@@ -7,9 +7,8 @@ import click
 import peewee
 
 import utils
-from models import BookIO, ScannedTextSimhash
+from models import ScannedTextSimhash
 from const import OUTPUT_MISC_DIR_PATH, DATETIME_SLUG
-from . import get_filtered_duplicates
 
 
 @click.command("step02-export-simhash-eval-sheet")
@@ -40,6 +39,8 @@ def step02_export_simhash_eval_sheet(n_samples: int, max_workers: int):
     Saved as:
     - `/data/output/misc/deduplication-simhash-eval-sheet-{n-samples}-{datetime}.csv`
     """
+    from . import get_filtered_duplicates
+
     output_filepath = Path(
         OUTPUT_MISC_DIR_PATH,
         f"deduplication-simhash-eval-sheet-{n_samples}-{DATETIME_SLUG}.csv",
@@ -55,7 +56,9 @@ def step02_export_simhash_eval_sheet(n_samples: int, max_workers: int):
     hashes_to_books = get_filtered_duplicates(max_workers)
 
     # Pick n_samples hashes
-    sampled_hashes = random.shuffle(list(hashes_to_books.keys()))[0:n_samples]
+    sampled_hashes = list(hashes_to_books.keys())
+    random.shuffle(sampled_hashes)
+    sampled_hashes = sampled_hashes[0:n_samples]
 
     #
     # Export samples
@@ -71,7 +74,7 @@ def step02_export_simhash_eval_sheet(n_samples: int, max_workers: int):
         writer.writerow(["simhash"] + [f"gbooks_url_{i}" for i in range(1, 21)])
 
         for simhash in sampled_hashes:
-            book = hashes_to_books[simhash]
+            books = hashes_to_books[simhash]
             gbooks_urls = []
 
             if samples_written >= n_samples:
@@ -120,6 +123,5 @@ def step02_export_simhash_eval_sheet(n_samples: int, max_workers: int):
 
     click.echo(f"📊 Books: {total_books}")
     click.echo(f"📊 Books w/ text and simhash: {total_books_with_hash}")
-    click.echo(f"📊 Unique books: {total_unique_books}")
     click.echo(f"📊 Unique books w/ at least 1 duplicate: {total_unique_books_with_duplicates}")
     click.echo(f"📊 Duplicate books: {total_duplicate_books}")
