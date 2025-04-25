0d3fde6a1857e8debb2b379c9401d87d6b0df0d7
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2025-04-25 16:49:10 -0400

    Remove layout-aware text experiment

--- commands/process/__init__.py
+++ commands/process/__init__.py
@@ -1,12 +1,7 @@
 import click
 
-from .layout_aware_text import layout_aware_text
-
 
 @click.group("process")
 def process():
     """Command group: Corpus processing / augmentation."""
     pass
-
-
-process.add_command(layout_aware_text)
